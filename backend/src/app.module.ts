55e13b666a272b2c2ad04df00918f0a7e5c0f9cf
Long <vohoanglong0002@gmail.com>
2026-07-02 23:26:44 +0700

    fix: register health check route for Render deploy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { Result } from './results/result.entity';
 import { UsersModule } from './users/users.module';
@@ -46,5 +48,7 @@ const getDatabaseConfig = (): TypeOrmModuleOptions => {
     ResultsModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
